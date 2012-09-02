7255e869d7b5eb712a007d93b48ffef201bed622
matt <matt@warrencomputing.net>
2012-09-02 08:15:07 -0400

    adding generated source with notes from Rails for Zombies tutorial.
    Nothing about this should really function.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waldo::Application.config.secret_token = 'b38039f4d4b22ac8e4f6e25f4248f9c6ebf8d9745c37d820c44ef1726d8d814c3cd76b6c7524668950ec26d4996d266d8e745b56873821161fa09663b236acd7'
