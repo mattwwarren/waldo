697c26f76f8e0203cbf4be9bf12a36b8000c537b
Matt Warren <mwarren@foobar.(none)>
2013-02-09 14:51:01 -0500

    date field is now a date picker

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $(document).on("focus", "[data-behaviour~='datepicker']", function(e){
+     $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
+  });
+});
