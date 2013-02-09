b6e7adb355fb199ac3c604bea9ff39bea0554b08
Matt Warren <mwarren@foobar.(none)>
2013-02-09 13:21:45 -0500

    Fixes #10 by adding multiple entries for multiple days in the subject line

--- extras/email_parser.rb
+++ extras/email_parser.rb
@@ -20,7 +20,11 @@ name = String.new
 emailaddr = String.new
 notes = String.new
 active_date = Date.new
+subjects = Array.new
+dates = Array.new
 text_next = false
+
+# Read in the email and parse out the lines we care about
 
 STDIN.each do |line|
   if line.match(/Subject: (.*)/)
@@ -34,15 +38,13 @@ STDIN.each do |line|
   end
 end
 
-if subject.match(/(today|tomorrow|next \w+|this \w+)/i)
-  active_date = Chronic.parse($1)
-elsif subject.match(/(mon\w*|tue\w*|wed\w*|thur\w*|fri\w*)/i)
-  active_date = Chronic.parse($1)
-else
-  active_date = Date.today
-end
+# Remove leading whitespace to clean up formatting and 
+# make further parsing easier
 
 messagetext.delete_if {|s| s.empty?}
+
+# Some (Most??) clients throw junk on the first line we get
+# Trim that off and move on
 
 if messagetext[0] =~ /--/
   messagetext.reverse!
@@ -50,15 +52,33 @@ if messagetext[0] =~ /--/
   messagetext.reverse!
 end
 
+# Next lines are content definitons, don't care
+
 messagetext.delete_if {|s| s.include?("Content-")}
+
+# Detect the end of the email by looking for a signature
+# We only want text above that so strip it off
 
 if messagetext.index {|s| s =~ /--\w*/}
   eom = messagetext.index {|s| s =~ /--\w*/} - 1
   messagetext = messagetext.slice(0..eom)
 end
 
+# Finally, parse through the other information we got
+# These variables will be used for storing to the db
+
 name = from.partition("<")[0]
 emailaddr = from.partition("<")[2][0...-1]
 notes = messagetext.join(" ")
 
-Users.create(:name => name, :email => emailaddr, :status => subject, :notes => notes, :active_date => active_date)
+# Handle multiple days off for the same reason
+# Multiple matches will result in arrays of arrays
+# Create a date we can use and save it to the db
+
+subjects = subject.scan(/(today|tomorrow|next \w+|this \w+|mon\w*|tue\w*|wed\w*|thur\w*|fri\w*)/i)
+subjects.each do |dates|
+  dates.each do |date|
+    active_date = Chronic.parse(date)
+    Users.create(:name => name, :email => emailaddr, :status => subject, :notes => notes, :active_date => active_date)
+  end
+end
